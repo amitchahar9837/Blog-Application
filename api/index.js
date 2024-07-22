1594a3501e9753ef540629c755c7d8499b41fec7
amitchahar9837 <amitchahar9837@gmail.com>
2024-07-22 22:34:55 +0530

    Connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+mongoose.connect(process.env.MONGOURI).then(()=>{
+      console.log("database connected")
+}).catch((err) =>{
+      console.log(err)
+})
 const app = express();
 
 app.listen(3000,()=>{
