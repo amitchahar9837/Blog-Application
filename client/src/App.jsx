46e161cb9817517d0b90310e968a161b5b69cbe5
amitchahar9837 <amitchahar9837@gmail.com>
2024-07-22 08:48:53 +0530

    Install react-router-dom and create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home";
+import About from './pages/About'
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1 className='text-red-400'>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
