350effa037845b45f1dbe9857f492a133a151017
amitchahar9837 <amitchahar9837@gmail.com>
2024-08-10 23:27:57 +0530

    some changes in vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,28 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
+  server:{
+    proxy:{
+      '/api':{
         target: 'http://localhost:3000',
         changeOrigin: true,
-        secure: false,
+        secure:false,
       }
     }
-  },
-  build: {
-    outDir: 'client/dist',  // This ensures the build output goes to 'client/dist'
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Increase the chunk size limit to 1000 kB
   },
   plugins: [react()],
 })
