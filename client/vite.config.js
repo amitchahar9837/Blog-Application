27a2d66a254036b2777643788010446f9044c29d
amitchahar9837 <amitchahar9837@gmail.com>
2024-08-10 09:32:09 +0530

    changes in vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
+  server: {
+    proxy: {
+      '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
-        secure:false,
+        secure: false,
       }
     }
+  },
+  build: {
+    outDir: 'dist',  // This ensures the build output goes to 'client/dist'
   },
   plugins: [react()],
 })
