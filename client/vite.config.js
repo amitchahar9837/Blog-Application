a33516d85247b6eeff5722f4c1825faf45e112e1
amitchahar9837 <amitchahar9837@gmail.com>
2024-08-10 20:48:11 +0530

    changes in vite config file to remove a error

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -14,6 +13,16 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',  // This ensures the build output goes to 'client/dist'
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase the chunk size limit to 1000 kB
   },
   plugins: [react()],
 })
